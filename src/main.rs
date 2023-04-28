8ab29b4b4608b753842320bfaa8b180f741724ac
Raphaël Zumer <rzumer@tebako.net>
2023-04-28 19:32:50 -0400

    Add further character replacement constraints
    * Ignore outputs with invalid items (rejected in game)
    * Add exact-match --flags option (must be 5-digit binary)
    * Add --keep-checksum option to not replace the checksum character

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 mod dq1;
 use std::env;
 
-fn process_dq1(input: &str, name: Option<String>) -> Result<String, String> {
+fn process_dq1(
+    input: &str,
+    name: Option<String>,
+    progress_flags: Option<u8>,
+    keep_checksum: bool,
+) -> Result<String, String> {
     if let Ok(result) = dq1::decode_jumon(input) {
         let data = dq1::GameData::from_bytes(result.as_slice());
         return Ok(format!(
@@ -11,7 +16,8 @@ fn process_dq1(input: &str, name: Option<String>) -> Result<String, String> {
     }
 
     let mut substitutions: Vec<(String, dq1::GameData)> = Vec::new();
-    for input_index_to_replace in 0..input.len() {
+    let max_index = if keep_checksum { input.len() - 1 } else { input.len() };
+    for input_index_to_replace in 0..max_index {
         for moji in &dq1::JUMON_MOJI_TABLE {
             let mut new_string = String::with_capacity(input.len());
             for (input_index, input_character) in input.chars().enumerate() {
@@ -30,7 +36,19 @@ fn process_dq1(input: &str, name: Option<String>) -> Result<String, String> {
                         continue;
                     }
                 }
-                substitutions.push((new_formatted_string, data));
+                // If progress flags are specified, ignore any substitutions where they change.
+                if let Some(flags) = progress_flags {
+                    let decoded_flags =
+                        data.progress_flags.iter().fold(0, |acc, &b| (acc << 1) | b as u8);
+                    if flags != decoded_flags {
+                        continue;
+                    }
+                }
+                // Exclude codes that generate an invalid item.
+                if data.items.contains(&(dq1::ITEM_TABLE.len() as u8 - 1)) {
+                    continue;
+                }
+                substitutions.push((new_string.clone(), data));
             }
         }
     }
@@ -52,15 +70,33 @@ fn main() {
     args = args[1..].to_vec();
 
     if args.is_empty() {
-        eprintln!("usage: {} [--name <name>] <input>", program);
+        eprintln!("usage: {} [--name <name>] [--flags <flags>] [--keep-checksum] <input>", program);
         return;
     }
 
-    // Parse an optional name option to constrain substitutions.
+    // Parse optional options to constrain substitutions.
     let mut name: Option<String> = None;
-    if args.len() > 2 && (args[0] == "--name" || args[1] == "-n") {
-        name = Some(args[1].clone());
-        args = args[2..].to_vec();
+    let mut progress_flags: Option<u8> = None;
+    let mut keep_checksum: bool = false;
+    while args.len() >= 2 {
+        let arg: &str = &args[0];
+        match arg {
+            "--name" | "-n" => {
+                name = Some(args[1].clone());
+                args = args[2..].to_vec();
+            }
+            "--flags" | "-f" => {
+                progress_flags = args[1].clone().parse::<u8>().ok();
+                args = args[2..].to_vec();
+            }
+            "--keep-checksum" | "-k" => {
+                keep_checksum = true;
+                args = args[1..].to_vec();
+            }
+            _ => {
+                break;
+            }
+        }
     }
 
     // Join all arguments to account for any spacing within the password.
@@ -69,7 +105,7 @@ fn main() {
     match input_length {
         20 => {
             // DQ1
-            let result = process_dq1(input_string, name);
+            let result = process_dq1(input_string, name, progress_flags, keep_checksum);
             match result {
                 Ok(output) => println!("{}", output),
                 Err(err) => eprintln!("{}", err),
